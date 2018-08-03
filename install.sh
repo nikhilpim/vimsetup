933e399b25efd12984dfcbbafef4d59c962223ac
Nikhil Pimpalkhare <Nikhil_Pimpalkhare@intuit.com>
2018-08-03 14:43:20 -0700

    Added basic plugins to install

--- install.sh
+++ install.sh
@@ -8,6 +8,13 @@ fi
 
 cp "$HOME/vimsetup/.vimrc" "$HOME/.vimrc"
 
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+#gundo! undo tree visualizer
+git clone http://github.com/sjl/gundo.vim.git ~/.vim/bundle/gundo
+#nerd tree, for seeing that tree, ya dig?
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 
 
-
+echo "Make sure to open vim and run :Helptags in order to generate help docs
